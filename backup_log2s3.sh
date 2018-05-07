87809d88833dd595395943adfa002d4610951d0e
yinxiangApp <yinxiang@yinxiangdeMacBook-Air.local>
2018-05-07 17:33:55 +0900

    back up log

--- backup_log2s3.sh
+++ backup_log2s3.sh
@@ -6,9 +6,9 @@ TIMESTAMP="/tmp/${PROG}.log"
 DATE=`date +%Y-%m-%d --date '1 day ago'`
 DELETE_DATE=`date +%Y-%m-%d --date '7 day ago'`
 BUCKET="budousan-backup/honban/budousan-back"
+BACKUP_PATH="/opt/log/budousan/budousan-back"
 BACKUP_LIST=`ls ${BACKUP_PATH} | grep $DATE`
 DELETE_LIST=`ls ${BACKUP_PATH} | grep $DELETE_DATE`
-BACKUP_PATH="/opt/log/budousan/budousan-back/"
 
 ### error
 error(){
